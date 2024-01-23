cd40bbc7b5d7f64c10ad71fd3fac8874bf0f8b90
Fredi Kats <fredikats@outlook.com>
2024-01-23 21:13:14 +0100

    Add XML LeadingTrivia formatter

---
+++ Sources/Kysect.DotnetProjectSystem.Tests/Xml/XmlDocumentSyntaxFormatterTests.cs
@@ -0,0 +1,101 @@
+﻿using Kysect.DotnetProjectSystem.Xml;
+using Microsoft.Language.Xml;
+
+namespace Kysect.DotnetProjectSystem.Tests.Xml;
+
+public class XmlDocumentSyntaxFormatterTests
+{
+    private readonly XmlDocumentSyntaxFormatter _formatter = new XmlDocumentSyntaxFormatter();
+
+    [Fact]
+    public void Format_FormatterDocument_ReturnStringWithoutChanges()
+    {
+        var input = """
+                          <Project>
+                            <Node1>
+                              <Node2 />
+                            </Node1>
+                          </Project>
+                          """;
+
+        var expected = """
+                    <Project>
+                      <Node1>
+                        <Node2 />
+                      </Node1>
+                    </Project>
+                    """;
+
+        Validate(input, expected);
+    }
+
+    [Fact]
+    public void Format_NodeWithoutPrecedingTrivia_ReturnStringWithCorrectTrivia()
+    {
+        var expected = """
+                          <Project>
+                            <Node1>
+                              <Node2 />
+                            </Node1>
+                          </Project>
+                          """;
+
+        var input = """
+                       <Project>
+                       <Node1>
+                       <Node2 />
+                       </Node1>
+                       </Project>
+                       """;
+
+        Validate(input, expected);
+    }
+
+    [Fact]
+    public void Format_NodesInOneLine_ReturnStringWithCorrectTrivia()
+    {
+        var expected = """
+                       <Project>
+                         <Node1>
+                           <Node2 />
+                         </Node1>
+                       </Project>
+                       """;
+
+        var input = """
+                    <Project><Node1><Node2 /></Node1></Project>
+                    """;
+
+        Validate(input, expected);
+    }
+
+    [Fact]
+    public void Format_MultipleChild_ReturnStringWithCorrectTrivia()
+    {
+        var expected = """
+                       <Project>
+                         <Node1 />
+                         <Node2 />
+                         <Node3 />
+                       </Project>
+                       """;
+
+        var input = """
+                    <Project>
+                    <Node1 />
+                    <Node2 />
+                    <Node3 />
+                    </Project>
+                    """;
+
+        Validate(input, expected);
+    }
+
+    private void Validate(string input, string expected)
+    {
+        XmlDocumentSyntax document = Parser.ParseText(input);
+        XmlDocumentSyntax actual = _formatter.Format(document);
+
+        actual.ToFullString().Should().Be(expected);
+    }
+}
---
+++ Sources/Kysect.DotnetProjectSystem/Xml/XmlDocumentSyntaxFormatter.cs
@@ -0,0 +1,93 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.CommonLib.Exceptions;
+using Microsoft.Language.Xml;
+using System.Text;
+
+namespace Kysect.DotnetProjectSystem.Xml;
+
+public class XmlDocumentSyntaxFormatter
+{
+    public const string DefaultIndention = "  ";
+
+    public XmlDocumentSyntax Format(XmlDocumentSyntax documentSyntax)
+    {
+        documentSyntax.ThrowIfNull();
+
+        IXmlElement updatedElement = UpdateElement(documentSyntax.Root, 0);
+        documentSyntax = documentSyntax.ReplaceNode(
+            documentSyntax.Root.AsSyntaxElement.AsNode,
+            updatedElement.AsSyntaxElement.AsNode);
+
+        return documentSyntax;
+    }
+
+    private IXmlElement UpdateElement(IXmlElement currentElement, int depth)
+    {
+        currentElement.ThrowIfNull();
+
+        IXmlElementSyntax modified = currentElement.AsSyntaxElement;
+        modified = modified.RemoveAllChild();
+
+        foreach (IXmlElement? element in currentElement.Elements)
+        {
+            IXmlElement updatedElement = UpdateElement(element, depth + 1);
+
+            modified = modified
+                .AddChild(updatedElement.AsSyntaxElement);
+        }
+
+        return AddLeadingTrivia(depth, modified);
+    }
+
+    private IXmlElement AddLeadingTrivia(int depth, IXmlElementSyntax modified)
+    {
+        if (modified is XmlEmptyElementSyntax xmlEmptyElementSyntax)
+            return AddLeadingTrivia(depth, xmlEmptyElementSyntax);
+
+        if (modified is XmlElementSyntax xmlElementSyntax)
+            return AddLeadingTrivia(depth, xmlElementSyntax);
+
+        throw SwitchDefaultExceptions.OnUnexpectedType(modified);
+    }
+
+    private IXmlElement AddLeadingTrivia(int depth, XmlEmptyElementSyntax xmlEmptyElementSyntax)
+    {
+        string trivia = GetTrivia(depth);
+
+        xmlEmptyElementSyntax = xmlEmptyElementSyntax.ReplaceNode(
+            xmlEmptyElementSyntax.LessThanToken,
+            xmlEmptyElementSyntax.LessThanToken.WithLeadingTrivia(SyntaxFactory.WhitespaceTrivia(trivia)));
+
+        return xmlEmptyElementSyntax;
+    }
+
+    private IXmlElement AddLeadingTrivia(int depth, XmlElementSyntax xmlElementSyntax)
+    {
+        string trivia = GetTrivia(depth);
+        xmlElementSyntax.StartTag.WithLeadingTrivia(SyntaxFactory.WhitespaceTrivia(trivia));
+
+        // TODO: remove this hack for first node
+        if (depth != 0)
+        {
+            xmlElementSyntax = xmlElementSyntax.ReplaceNode(
+                xmlElementSyntax.StartTag,
+                xmlElementSyntax.StartTag.WithLeadingTrivia(SyntaxFactory.WhitespaceTrivia(trivia)));
+        }
+
+        xmlElementSyntax = xmlElementSyntax.ReplaceNode(
+            xmlElementSyntax.EndTag,
+            xmlElementSyntax.EndTag.WithLeadingTrivia(SyntaxFactory.WhitespaceTrivia(trivia)));
+
+        return xmlElementSyntax;
+    }
+
+    private string GetTrivia(int depth)
+    {
+        var sb = new StringBuilder(Environment.NewLine);
+
+        for (int i = 0; i < depth; i++)
+            sb = sb.Append(DefaultIndention);
+
+        return sb.ToString();
+    }
+}
