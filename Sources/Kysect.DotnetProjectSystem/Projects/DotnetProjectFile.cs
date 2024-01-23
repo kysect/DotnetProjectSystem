a2f8c488bb392c9d99ffb10b319fa3611309f47a
Fredi Kats <fredikats@outlook.com>
2024-01-23 18:09:36 +0100

    Add API for creating empty dotnet project file

---
+++ Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectFile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Language.Xml;
+
+namespace Kysect.DotnetProjectSystem.Projects;
+
+public class DotnetProjectFile
+{
+    private readonly XmlDocumentSyntax _content;
+
+    public DotnetProjectFile(XmlDocumentSyntax content)
+    {
+        _content = content;
+    }
+
+    public static DotnetProjectFile CreateEmpty()
+    {
+        string contentTemplate =
+            """
+            <Project>
+            </Project>
+            """;
+
+        return new DotnetProjectFile(Parser.ParseText(contentTemplate));
+    }
+
+    public string ToXmlString()
+    {
+        return _content.ToFullString();
+    }
+}
