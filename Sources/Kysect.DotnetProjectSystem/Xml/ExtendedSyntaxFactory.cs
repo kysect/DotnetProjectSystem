3a42edcd9a84e5ddbe3d4edae4d4220254220c22
Fredi Kats <fredikats@outlook.com>
2024-01-23 21:41:32 +0100

    Replace manual trivia formatting with formatter usage

--- Sources/Kysect.DotnetProjectSystem/Xml/ExtendedSyntaxFactory.cs
+++ Sources/Kysect.DotnetProjectSystem/Xml/ExtendedSyntaxFactory.cs
@@ -1,42 +1,29 @@
 ﻿using Microsoft.Language.Xml;
-using System.Text;
 
 namespace Kysect.DotnetProjectSystem.Xml;
 
 public static class ExtendedSyntaxFactory
 {
-    public const string DefaultIndention = "  ";
-
     public static XmlNameSyntax XmlName(string name)
     {
         return SyntaxFactory.XmlName(null, SyntaxFactory.XmlNameToken(name, null, null));
     }
 
-    public static XmlElementSyntax XmlElement(string name, int depth)
+    public static XmlElementSyntax XmlElement(string name)
     {
         var startTagSyntax = SyntaxFactory.XmlElementStartTag(
-            SyntaxFactory.Punctuation(SyntaxKind.LessThanToken, "<", XmlWhiteIndention(depth), null),
+            SyntaxFactory.Punctuation(SyntaxKind.LessThanToken, "<", null, null),
             XmlName(name),
             SyntaxFactory.List<XmlAttributeSyntax>(),
             SyntaxFactory.Punctuation(SyntaxKind.GreaterThanToken, ">", SyntaxFactory.WhitespaceTrivia(string.Empty), null)
         );
 
         var endTagSyntax = SyntaxFactory.XmlElementEndTag(
-            SyntaxFactory.Punctuation(SyntaxKind.LessThanSlashToken, "</", XmlWhiteIndention(depth), null),
+            SyntaxFactory.Punctuation(SyntaxKind.LessThanSlashToken, "</", null, null),
             XmlName(name),
             SyntaxFactory.Punctuation(SyntaxKind.GreaterThanToken, ">", SyntaxFactory.WhitespaceTrivia(string.Empty), null)
         );
 
         return SyntaxFactory.XmlElement(startTagSyntax, SyntaxFactory.List<SyntaxNode>(), endTagSyntax);
-    }
-
-    public static SyntaxTrivia XmlWhiteIndention(int depth)
-    {
-        var sb = new StringBuilder(Environment.NewLine);
-
-        for (int i = 0; i < depth; i++)
-            sb = sb.Append(DefaultIndention);
-
-        return SyntaxFactory.WhitespaceTrivia(sb.ToString());
     }
 }
