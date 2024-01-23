f7b61f98a1b60deeebdc972e16bdd35bb38ae385
Fredi Kats <fredikats@outlook.com>
2024-01-23 08:42:29 +0100

    Add API for generating dotnet solution file template

---
+++ Sources/Kysect.DotnetProjectSystem.Tests/Generating/SolutionFileStringBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using Kysect.DotnetProjectSystem.Generating;
+
+namespace Kysect.DotnetProjectSystem.Tests.Generating;
+
+public class SolutionFileStringBuilderTests
+{
+    private readonly SolutionFileStringBuilder _sut;
+
+    public SolutionFileStringBuilderTests()
+    {
+        _sut = new SolutionFileStringBuilder();
+    }
+
+    [Fact]
+    public void Build_WithoutModification_ReturnTemplateString()
+    {
+        var expected = """
+                       Microsoft Visual Studio Solution File, Format Version 12.00
+                       # Visual Studio Version 17
+                       VisualStudioVersion = 17.9.34310.174
+                       MinimumVisualStudioVersion = 10.0.40219.1
+                       Global
+                         GlobalSection(SolutionProperties) = preSolution
+                           HideSolutionNode = FALSE
+                         EndGlobalSection
+                       EndGlobal
+                       """;
+
+        string actual = _sut.Build();
+
+        actual.Should().Be(expected);
+    }
+}
---
+++ Sources/Kysect.DotnetProjectSystem/Generating/SolutionFileStringBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Kysect.DotnetProjectSystem.Generating;
+
+public class SolutionFileStringBuilder
+{
+    private readonly StringBuilder _builder;
+
+    public SolutionFileStringBuilder()
+    {
+        const string header = """
+                              Microsoft Visual Studio Solution File, Format Version 12.00
+                              # Visual Studio Version 17
+                              VisualStudioVersion = 17.9.34310.174
+                              MinimumVisualStudioVersion = 10.0.40219.1
+                              """;
+
+        _builder = new StringBuilder();
+        _builder.AppendLine(header);
+    }
+
+    public string Build()
+    {
+        const string footer = """
+                              Global
+                                GlobalSection(SolutionProperties) = preSolution
+                                  HideSolutionNode = FALSE
+                                EndGlobalSection
+                              EndGlobal
+                              """;
+
+        _builder.Append(footer);
+        return _builder.ToString();
+    }
+}
