a5f969cd6ed33db1c1ac78a9a7d78a55d33a7434
Fredi Kats <fredikats@outlook.com>
2024-01-23 21:13:14 +0100

    Add XML LeadingTrivia formatter

---
+++ Sources/Kysect.DotnetProjectSystem/Xml/XmlElementSyntaxExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Microsoft.Language.Xml;
+
+namespace Kysect.DotnetProjectSystem.Xml;
+
+public static class XmlElementSyntaxExtensions
+{
+    public static IXmlElementSyntax RemoveAllChild(this IXmlElementSyntax xmlElementSyntax)
+    {
+        xmlElementSyntax.ThrowIfNull();
+
+        while (xmlElementSyntax.Elements.Any())
+            xmlElementSyntax = xmlElementSyntax.RemoveChild(xmlElementSyntax.Elements.First());
+
+        return xmlElementSyntax;
+    }
+}
