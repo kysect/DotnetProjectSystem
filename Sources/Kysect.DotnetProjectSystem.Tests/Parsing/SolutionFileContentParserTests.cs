eb9e71e6e6958605fad358bb6ffad4b0e81b3d5e
Fredi Kats <fredikats@outlook.com>
2024-01-26 19:57:25 +0100

    Incapsulate assertion logic for solution parser

--- Sources/Kysect.DotnetProjectSystem.Tests/Parsing/SolutionFileContentParserTests.cs
+++ Sources/Kysect.DotnetProjectSystem.Tests/Parsing/SolutionFileContentParserTests.cs
@@ -7,38 +7,39 @@ namespace Kysect.DotnetProjectSystem.Tests.Parsing;
 public class SolutionFileContentParserTests
 {
     private readonly SolutionFileContentParser _parser;
+    private readonly MockFileSystem _fileSystem;
 
     public SolutionFileContentParserTests()
     {
         _parser = new SolutionFileContentParser();
+        _fileSystem = new MockFileSystem();
     }
 
     [Fact]
     public void ParseSolutionFileContent_ThisSolution_ReturnExpectedResult()
     {
         var solutionBuilder = new SolutionFileStructureBuilder("Solution")
-            .AddProject(new ProjectFileStructureBuilder("Kysect.DotnetSlnParser", string.Empty))
-            .AddProject(new ProjectFileStructureBuilder("Kysect.DotnetSlnParser.Tests", string.Empty));
+            .AddProject(new ProjectFileStructureBuilder("Kysect.DotnetSlnParser"))
+            .AddProject(new ProjectFileStructureBuilder("Kysect.DotnetSlnParser.Tests"));
 
-        var fileSystem = new MockFileSystem();
-        string solutionFile = solutionBuilder.CreateSolutionFile(fileSystem);
+        string solutionFile = solutionBuilder.CreateSolutionFile(_fileSystem);
 
         List<DotnetProjectFileDescriptor> expected = new List<DotnetProjectFileDescriptor>
         {
             new DotnetProjectFileDescriptor(
                 Guid.Empty,
                 "Kysect.DotnetSlnParser",
-                fileSystem.Path.Combine("Kysect.DotnetSlnParser", "Kysect.DotnetSlnParser.csproj"),
+                _fileSystem.Path.Combine("Kysect.DotnetSlnParser", "Kysect.DotnetSlnParser.csproj"),
                 Guid.Empty),
 
             new DotnetProjectFileDescriptor(
                 Guid.Empty,
                 "Kysect.DotnetSlnParser.Tests",
-                fileSystem.Path.Combine("Kysect.DotnetSlnParser.Tests", "Kysect.DotnetSlnParser.Tests.csproj"),
+                _fileSystem.Path.Combine("Kysect.DotnetSlnParser.Tests", "Kysect.DotnetSlnParser.Tests.csproj"),
                 Guid.Empty),
         };
 
-        List<DotnetProjectFileDescriptor> projects = _parser.ParseSolutionFileContent(solutionFile).ToList();
+        IReadOnlyCollection<DotnetProjectFileDescriptor> projects = _parser.ParseSolutionFileContent(solutionFile);
 
         projects.Should().BeEquivalentTo(expected);
     }
