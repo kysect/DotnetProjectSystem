f2995943928ab008ff21af571bc0e9f16233869c
Fredi Kats <fredikats@outlook.com>
2024-01-26 08:47:31 +0100

    Implement source file finder

--- Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectFileConstant.cs
+++ Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectFileConstant.cs
@@ -4,4 +4,5 @@ public static class DotnetProjectFileConstant
 {
     public const string PropertyGroup = "PropertyGroup";
     public const string ToolsVersion = "ToolsVersion";
+    public const string EnableDefaultItems = "EnableDefaultItems";
 }
