ba188fa248ba6570fcf6a99819b769921b7de38b
Fredi Kats <fredikats@outlook.com>
2024-01-27 14:46:23 +0100

    Add API for enabling CPM in Directory.Packages.props file

---
+++ Sources/Kysect.DotnetProjectSystem/Projects/DirectoryBuildPropsFile.cs
@@ -0,0 +1,11 @@
+﻿namespace Kysect.DotnetProjectSystem.Projects;
+
+public class DirectoryBuildPropsFile
+{
+    public DotnetProjectFile File { get; }
+
+    public DirectoryBuildPropsFile(DotnetProjectFile file)
+    {
+        File = file;
+    }
+}
