8dd1068088ec7b0e9fd4b4dd948a8ac49d2fa5f5
Fredi Kats <fredikats@outlook.com>
2024-01-27 17:55:17 +0100

    Add API for getting package versions from Directory.Packages.props

--- Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectFileConstant.cs
+++ Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectFileConstant.cs
@@ -5,6 +5,7 @@ public static class DotnetProjectFileConstant
     public const string PropertyGroup = "PropertyGroup";
     public const string ItemGroup = "ItemGroup";
     public const string PackageReference = "PackageReference";
+    public const string PackageVersion = "PackageVersion";
     public const string ToolsVersion = "ToolsVersion";
     public const string EnableDefaultItems = "EnableDefaultItems";
 }
