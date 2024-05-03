a8705ca7c0e836508e3aeb792ec92a9c63aab4a4
Fredi Kats <fredikats@outlook.com>
2024-05-03 22:49:46 +0200

    Add Directory.Build.targets file parsing

--- Sources/Kysect.DotnetProjectSystem/Tools/SolutionItemNameConstants.cs
+++ Sources/Kysect.DotnetProjectSystem/Tools/SolutionItemNameConstants.cs
@@ -4,4 +4,5 @@ public static class SolutionItemNameConstants
 {
     public const string DirectoryBuildProps = "Directory.Build.props";
     public const string DirectoryPackagesProps = "Directory.Packages.props";
+    public const string DirectoryBuildTargets = "Directory.Build.targets";
 }
