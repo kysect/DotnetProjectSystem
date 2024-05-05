9e4e4a162fdd14f006d5234b90a70e3e6877886d
Fredi Kats <fredikats@outlook.com>
2024-05-05 13:36:31 +0200

    Use default library project id instead of Guild.Empty

---
+++ Sources/Kysect.DotnetProjectSystem/Projects/DotnetProjectTypeIds.cs
@@ -0,0 +1,6 @@
+﻿namespace Kysect.DotnetProjectSystem.Projects;
+
+public static class DotnetProjectTypeIds
+{
+    public static Guid DefaultLibrary { get; } = Guid.Parse("9A19103F-16F7-4668-BE54-9A1E7A4F7556");
+}
